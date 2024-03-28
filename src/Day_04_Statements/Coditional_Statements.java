95fcaa96fc966d1feaaf6bbd647e2e8d6649fa7c
Anil Kumar <khan.testing1994@gmail.com>
2024-03-28 13:16:22 +0530

    just for testing

--- src/Day_04_Statements/Coditional_Statements.java
+++ src/Day_04_Statements/Coditional_Statements.java
@@ -3,7 +3,8 @@ package Day_04_Statements;
 public class Coditional_Statements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("hello github");
 
 	}
 
